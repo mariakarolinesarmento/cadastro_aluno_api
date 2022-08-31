a4b082c791713133756b6f50e0fad3fdc8cc1e40
Karoline Sarmento <100390180+mariakarolinesarmento@users.noreply.github.com>
2022-08-30 21:29:20 -0300

    Add files via upload

---
+++ cadastro_alunos_api/src/test/java/com/example/cadastro_alunos_api/CadastroAlunosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cadastro_alunos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroAlunosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ cadastro_alunos_api/target/classes/db/migration/V01_criar_e_registrar_alunos.sql
@@ -0,0 +1,10 @@
+CREATE TABLE aluno (
+	codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50) NOT NULL,
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO pessoa (nome) values ('João Silva');
+INSERT INTO pessoa (nome) values ('Maria');
+INSERT INTO pessoa (nome) values ('Paulo');
+)
